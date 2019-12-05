695bec6a1170bd58b846ee1f206fee3a7e5aa35c
Abdullah Alghamdi <abdullah@manzilak.com>
2019-12-05 22:56:40 +0300

    Update WeIrD-StRiNg-CaSe.js

--- WeIrD-StRiNg-CaSe.js
+++ WeIrD-StRiNg-CaSe.js
@@ -1,8 +1,7 @@
 function toWeirdCase(string) {
-  const chars = string.split('');
   var after = '';
   var odd = 0;
-  chars.map((v,i) => {
+  string.split('').map((v,i) => {
     v === ' ' ? odd = 1 : odd = odd;
     odd % 2 === 0 ? after += v.toUpperCase() : after += v.toLowerCase();
     odd += 1;
